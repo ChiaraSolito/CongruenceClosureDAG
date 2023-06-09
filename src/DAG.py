abafd7a10af70d6a6004208004462e37cb8dc118
ChiaraSolito <chiara.solito00@gmail.com>
2023-06-09 18:00:26 +0200

    updated union and ccpar

--- src/DAG.py
+++ src/DAG.py
@@ -12,28 +12,34 @@ class DAG:
 
     def add_node(self, fn, args:list):
         id = self.newid()
-        mutable_ccpar = set()
-        mutable_find = []
-        mutable_find.append(id)
-        self.g.add_node(id,fn=fn, args=args, mutable_find=mutable_find,mutable_ccpar=mutable_ccpar)
+        m_ccpar = set()
+        m_find = []
+        m_find.append(id)
+        self.g.add_node(id,fn=fn, args=args, m_find=m_find,m_ccpar=m_ccpar)
     
     def node(self, i:int):
         return self.g.nodes[i]
     
     def find(self, i:int):
         n = self.node(i)
-        if n["mutable_find"] == i: 
+        if n["m_find"] == i: 
             return i
         else:
-            self.find(n["mutable_find"]) 
+            self.find(n["m_find"]) 
     
     def union(self, i1:int, i2:int):
-        return 
+        n1 = self.find(i1)
+        n2 = self.find(i2)
+
+        n1["find"] = n2["find"]
+        n2["m_ccpar"].update(n1["m_ccpar"])
+        n1["m_ccpar"].clear()
     
-    def ccpar(i:int):
-        return
+    def ccpar(self, i:int):
+        return self.node(self.find(i))["m_ccpar"]
     
     def congruent(self, i1:int, i2:int):
+        
         return 
     
     def merge(self, i1:int, i2:int):
