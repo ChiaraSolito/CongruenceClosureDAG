0475195952f510012ec8aefaf83aecacf27cab40
ChiaraSolito <chiara.solito00@gmail.com>
2023-06-14 11:48:23 +0200

    partially finished DAG class

--- src/DAG.py
+++ src/DAG.py
@@ -31,7 +31,7 @@ class DAG:
         n1 = self.find(i1)
         n2 = self.find(i2)
 
-        n1["find"] = n2["find"]
+        n1["m_find"] = n2["m_find"]
         n2["m_ccpar"].update(n1["m_ccpar"])
         n1["m_ccpar"].clear()
     
@@ -39,10 +39,30 @@ class DAG:
         return self.node(self.find(i))["m_ccpar"]
     
     def congruent(self, i1:int, i2:int):
-        
-        return 
+        ris = True
+        n1 = self.node(i1)
+        n2 = self.node(i2)
+        if n1["fn"]== n2["fn"] and len(n1["args"]) == len(n2["args"]):
+            for a1 in n1["args"]:
+                if ris:
+                    ris = False
+                    for a2 in n2["args"]:
+                        if self.find(a1) == self.find(a2):
+                            ris = True
+        else:
+            return False
+        return ris
     
     def merge(self, i1:int, i2:int):
+        if self.find(i1) != self.find(i2):
+            Pi1 = self.ccpar(i1)
+            Pi2 = self.ccpar(i2)
+            union = self.union(i1,i2)
+
+            for t1 in Pi1:
+                for t2 in Pi2:
+                    if self.find(t1) != self.find(t2) or self.congruent(t1,t2):
+                        self.merge(t1,t2)
         return 
 
 
