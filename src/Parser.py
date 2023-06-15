e99948dcb081971e337908b8878d521d1eb6c41b
ChiaraSolito <chiara.solito00@gmail.com>
2023-06-15 16:28:07 +0200

    added equation parsing

--- src/Parser.py
+++ src/Parser.py
@@ -1,13 +1,48 @@
 import re
-
+from src import DAG
 
 class Parser:
 
-    def __init__(self, data, d):
-        self.nodes = {}
+    def __init__(self, data, dag: DAG):
+        self.atom_dict = {}
+        self.priginal_data = data
+        self.dag = dag
         self.diseq = []
         self.eq = []
-        self.parse_data(data, d)
+
+    def parse_equations(self, data):
+
+        for element in data:
+
+            if "!=" in element:
+
+                couple = re.split('!=', element)
+                self.dag.add_node(couple[0])
+                self.dag.add_node(couple[1])
+
+                couple[0] = hash(couple[0].strip())
+                couple[1] = hash(couple[1].strip())
+
+                self.diseq.append(couple)
+
+            elif "=" in element:
+                couple = re.split('=', element)
+
+                self.dag.add_node(couple[0])
+                self.dag.add_node(couple[1])
+
+                couple[0] = hash(couple[0].strip())
+                couple[1] = hash(couple[1].strip())
+
+                self.eq.append(couple)
+
+            else:
+                self.add_node(element)
+                self.add_node("tt")
+                couple[0] = hash(element.strip())
+                couple[1] = hash("tt")
+
+                self.eq.append(couple)
 
     def parse(self, data):
         return
