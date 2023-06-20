f809a9d3b263fc458320bd4ec118a830ab330dbb
ChiaraSolito <chiara.solito00@gmail.com>
2023-06-20 12:03:30 +0200

    added input example

--- main.py
+++ main.py
@@ -1,44 +1,63 @@
-from src import dag
-from src import dag_builder
-from src import smt_parser
-import sys
+from src.dag import DAG
+from src.parser import Parser
+from src.smt_parser import SmtParser
 import time
-from pathlib import Path
+import sys 
 
 def process(file:str):
-    path = Path(file)
-    return path.read_text()
+    with open(file) as f:
+        return [line.rstrip('\n') for line in f.readlines()]
 
 def main(name:str):
+    #start clock
+    start = time.perf_counter()
 
     if len(name)<1: 
         print("ERROR: no file in input.")
     else:
         file = "data/" + name
 
-    if (not file.endswith(".txt")) or (not file.endswith('.smt2')): 
+    if not file.endswith(".txt") and not file.endswith('.smt2'): 
         print("ERROR: file are accepted in the form of text files or smt2 format only! Please check your input.")
         exit()
 
     #instantiate classes
     
     if file.endswith('.smt2'):
-        parser = smt_parser()
+        parser = SmtParser()
         solver = parser.parse(file)
     else:
-        parser = dag_builder()
+        dag = DAG()
+        parser = Parser(dag)
         input = process(file)
-        solver = parser.parse_equations(input)
+        parser.parse_formula(input)
+
+        # merge of all node that are in equivalence relation
+        satisfable = True
+        for coppiaeq in dag.equalities:
+            if satisfable:
+                satisfable = dag.merge(dag.g.nodes[coppiaeq[0]], dag.g.nodes[coppiaeq[1]]) 
+                #non funziona così perché i miei nodi non sono identificati da questo
+                pass
+
+        if satisfable:
+            print('The formula is satisfiable! You can look at the graph generated.')
+            #end clock
+            end = time.perf_counter()
+            print("Runned in :", end-start)
+            dag.print_graph()
+        else:
+            print('The formula is unsatisfiable! We will now terminate the execution.')
+            #end clock
+            end = time.perf_counter()
+            print("Runned in :", end-start)
+
 
 
 if __name__ == "__main__":
 
-    #start clock
-    start = time.clock()
-
     #start procedure
-    main(sys.argv[1])
-    
-    #end clock
-    end = time.clock()
-    print("Finished! Run in :", end-start)
+    if len(sys.argv)>1:
+        main(sys.argv[1])    
+    else:
+        print('Please insert an input.')
